90b04e3550a235e044ee77992c28e2ad7ef8dd83
Enrique Garcia <egarcialopez@gmail.com>
2024-05-02 17:16:20 +0200

    User creation

--- db/db.go
+++ db/db.go
@@ -24,15 +24,29 @@ func InitDB() {
 }
 
 func createTables() {
+
+	createUsersTableQuery := `CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL)`
+
+	_, err := DB.Exec(createUsersTableQuery)
+
+	if err != nil {
+		panic("Could not create users table")
+	}
+
 	createEventsTableQuery := `CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER)`
+		user_id INTEGER,
+		FOREIGN KEY(user_id) REFERENCES users(id)
+		)`
 
-	_, err := DB.Exec(createEventsTableQuery)
+	_, err = DB.Exec(createEventsTableQuery)
 
 	if err != nil {
 		panic("Could not create event table")
